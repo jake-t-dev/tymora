6c7b13426cd15d70edbcb1bfb155d041c9c78400
jake-t-dev <krysis15@outlook.com>
2026-01-09 14:27:16 +0000

    update test command

--- internal/bot/handlers/test.go
+++ internal/bot/handlers/test.go
@@ -10,6 +10,6 @@ func test(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "!test" {
 
-		s.ChannelMessageSend(m.ChannelID, "test")
+		s.ChannelMessageSend(m.ChannelID, "I am live! See my repository at https://github.com/jake-t-dev/tymora")
 	}
 }
